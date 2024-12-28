366282dc6037f3b79398c0897cf2e9382004be92
Okman-0920 <nohoksun94@gmail.com>
2024-12-29 00:32:25 +0900

    detail console

--- src/main/java/com/ll/jap/global/initData/BaseInitData.java
+++ src/main/java/com/ll/jap/global/initData/BaseInitData.java
@@ -19,13 +19,8 @@ public class BaseInitData {
             if (postService.count() > 0) return;
 
             Post post1 = postService.write("제목1", "내용1");
-            System.out.println(post1.getId() + "번 글이 생성되었습니다.");
-
             Post post2 = postService.write("제목2", "내용2");
-            System.out.println(post2.getId() + "번 글이 생성되었습니다.");
-
             Post post3 = postService.write("제목3", "내용3");
-            System.out.println(post3.getId() + "번 글이 생성되었습니다.");
         };
     }
 }
